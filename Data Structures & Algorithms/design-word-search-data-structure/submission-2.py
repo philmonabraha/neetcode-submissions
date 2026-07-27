7b2b4f1a587b6946f97b4a60c9c9f98e7e891a2a
philmonabraha <philmon_abraha@berkeley.edu>
2026-07-26 19:37:59 -0700

    Add: design-word-search-data-structure - submission-2

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-2.py
@@ -0,0 +1,59 @@
+
+class Tree():
+
+    def __init__(self, val = "", children = None, stop = False):
+
+        self.val = ""
+        if children == None:
+            self.children = {}
+        else:
+            self.children = children
+        self.stop = stop
+
+
+class WordDictionary:
+
+    def __init__(self):
+
+        self.tree = Tree()
+        
+    def addWord(self, word: str) -> None:
+
+        pointer = self.tree
+
+        for i in range(len(word)):
+            w = word[i]
+
+            if w not in pointer.children:
+                if i == len(word) - 1:
+                    pointer.children[w] = Tree(val=w, stop = True)
+                else:
+                    pointer.children[w] = Tree(val=w)
+            
+            pointer = pointer.children[w]
+
+        pointer.stop = True  
+   
+    def search(self, word: str) -> bool:
+        
+        def dfs(j, root):
+
+            pointer = self.tree
+
+            for i in range(len(word)):
+                w = word[i]
+                if w != "." and w not in pointer.children:
+                    return False
+                
+                elif w == ".":
+
+                    for child in pointer.children.values():
+                        if dfs(i+1, child):
+                            return True
+                                     
+                    return False              
+            
+            return pointer.stop
+
+        return dfs(0, self.root)
+ 
