817eae55e84c091480dcbcecd561c5bf9c27da7a
philmonabraha <philmon_abraha@berkeley.edu>
2026-07-30 10:42:07 -0700

    Add: islands-and-treasure - submission-2

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-2.py
@@ -0,0 +1,36 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+
+        visited = set()
+
+        def bfs(x, y):
+
+            directions = [[1, 0], [-1, 0], [0,1], [0, -1]]
+
+            queue = deque([(x,y)])
+
+            distance = 1
+
+            while queue:
+
+                for l in range(len(queue)):
+                    node = queue.popleft()
+                    for direc in directions:
+                        i, j = node[0] + direc[0], node[1] +direc[1]     
+
+                        if grid[i][j] != 0 and grid[i][j] != -1:
+                            grid[i][j] = min(distance, grid[i][j])
+                            queue.append((i,j))
+
+                distance += 1
+              
+        for i in range(len(grid)):
+
+            for j in range(len(grid[0])):
+
+                if grid[i][j] == 0 and grid[i][j] not in visited:
+                    visited.add((i,j))
+                    bfs(i, j)
+
+
+        
