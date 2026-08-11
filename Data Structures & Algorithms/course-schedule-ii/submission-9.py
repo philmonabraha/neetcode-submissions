3a20103b1c30015c0fd0c37f437bdad8dd8ff0c2
philmonabraha <philmon_abraha@berkeley.edu>
2026-08-11 10:19:20 -0700

    Add: course-schedule-ii - submission-9

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-9.py
@@ -0,0 +1,37 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+
+        premap = {}
+        for i in range(numCourses):
+            premap[i] = []
+
+        for pre, crs in prerequisites:
+            premap[crs].append(pre)
+
+        visit = set()
+        visited = set()
+        path = []
+
+        def dfs(crs):
+
+            if crs in visit:
+                return False
+            if premap[crs] == []:
+                if crs not in visited:
+                    path.append(crs)
+                return True
+
+            visit.add(crs)
+            for pre in premap[crs]:
+                if not dfs(pre):
+                    return False
+            visit.remove(crs)
+            premap[crs] = []
+
+            return True
+        
+        for i in range(numCourses):
+            if not dfs(i):
+                return []
+        
+        return path
