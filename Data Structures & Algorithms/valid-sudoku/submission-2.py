f6bdf638471e378b10bc4d183c47f5ed8d545240
philmonabraha <philmon_abraha@berkeley.edu>
2026-08-28 20:49:29 -0700

    Update: valid-sudoku - submission-2

--- Data Structures & Algorithms/valid-sudoku/submission-2.py
+++ Data Structures & Algorithms/valid-sudoku/submission-2.py
@@ -1,52 +1,53 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
 
-
-        for row in range(len(board)): 
-            seen = set()
-            for col in board[row]:
-
-                if col != '.':
-                    seen.add(col)
-                if col in seen:
-                    return False   
-
-        for i in range(9):
-            seen = set()
-            for j in range(9):
-
-                if board[j][i] != '.':
-                    seen.add(board[j][i])
-
-                if board[j][i] in seen:
+        for row in board:
+            items = set()
+            for num in row:
+                if num in items:
                     return False
+                if num == ".":
+                    continue
+                items.add(num)
         
-        for rows in range(0,9, 3):
+        for i in range(len(board)):
+            items = set()
+            for j in range(len(board[0])):
+                if board[j][i] in items:
+                    return False
+                if board[j][i] == ".":
+                    continue
+                items.add(board[j][i])
 
-            for cols in range(0, 9, 3):
-
-                seen = set()
-
-                for i in range(row, row+3):
-                    for j in range(cols, cols+3):
-
-                        if board[i][j] != '.':
-                            seen.add(board[j][i])
-
-                        if board[j][i] in seen:
+        
+        for i in range(0, 9, 3):
+            for j in range(0, 9, 3):
+                items = set()           
+                for k in range(3):
+                    for z in range(3):
+                        x = board[i+k][j+z]
+                        if x in items:
                             return False
-
-                    
-
-
-
+                        if x == ".":
+                            continue                        
+                        items.add(x)
 
 
         return True
 
 
+                    
 
-                
+
+
+        
+
+
+
+
+
+
+
 
 
 
