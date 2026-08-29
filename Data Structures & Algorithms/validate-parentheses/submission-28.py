85361ec68e7fda50e44630d2780d08e71560e05a
philmonabraha <philmon_abraha@berkeley.edu>
2026-08-29 09:30:20 -0700

    Add: validate-parentheses - submission-28

---
+++ Data Structures & Algorithms/validate-parentheses/submission-28.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+
+
+        stack = []
+
+        for par in s:
+
+            if par == "]":
+                if not stack or stack.pop() != "[":
+                    return False
+            elif par == "}":
+                if not stack or stack.pop() != "{":
+                    return False            
+
+            elif par == ")":
+                if not stack or stack.pop() != "(":
+                    return False
+            else:
+                stack.append(par)
+        
+        return len(stack) == 0
+
+        
