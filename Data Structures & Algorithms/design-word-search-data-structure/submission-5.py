db8fa4808dc267f026b0d2f46f3b5db774b956b2
philmonabraha <philmon_abraha@berkeley.edu>
2026-09-06 20:38:26 -0700

    Update: design-word-search-data-structure - submission-5

--- Data Structures & Algorithms/design-word-search-data-structure/submission-5.py
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-5.py
@@ -1,62 +1,56 @@
+class Tree:
 
-class Tree():
-
-    def __init__(self, val = "", children = None, stop = False):
-
-        self.val = ""
-        if children == None:
-            self.children = {}
-        else:
-            self.children = children
-        self.stop = stop
+    def __init__(self, val = "", end = False):
+        self.val = val
+        self.children = {}
+        self.end = end
 
 
 class WordDictionary:
 
     def __init__(self):
 
-        self.tree = Tree()
-        
+        self.tree = Tree()    
+
     def addWord(self, word: str) -> None:
 
         pointer = self.tree
 
-        for i in range(len(word)):
-            w = word[i]
-
+        for w in word:
             if w not in pointer.children:
-                if i == len(word) - 1:
-                    pointer.children[w] = Tree(val=w, stop = True)
-                else:
-                    pointer.children[w] = Tree(val=w)
-            
+                pointer.children[w] = Tree(val=w)
+        
             pointer = pointer.children[w]
 
-        pointer.stop = True  
-   
+        pointer.end = True
+
     def search(self, word: str) -> bool:
-        
-        def dfs(j, root):
 
-            pointer = self.tree
+        def dfs(index, root):
 
-            for i in range(j, len(word)):
+            pointer = root
+
+            for i in range(index, len(word)):
+
                 w = word[i]
-                
+        
                 if w == ".":
-                    for child in pointer.children.values():
+                    for child in pointer.children.values():               
                         if dfs(i+1, child):
                             return True
+                    return False
 
-                    return False   
                 else:
-                    
                     if w not in pointer.children:
-                        return False  
-                    
-                    pointer = pointer.children[w]         
-            
-            return pointer.stop
+                        return False
+                    pointer = pointer.children[w]
+
+            return pointer.end
 
         return dfs(0, self.tree)
- 
+
+
+
+
+
+        
