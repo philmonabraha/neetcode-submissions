53ecd4bc7a34b0e1cd075ced7c72660e2568c5fa
philmonabraha <philmon_abraha@berkeley.edu>
2026-09-20 18:49:01 -0700

    Update: reverse-nodes-in-k-group - submission-2

--- Data Structures & Algorithms/reverse-nodes-in-k-group/submission-2.py
+++ Data Structures & Algorithms/reverse-nodes-in-k-group/submission-2.py
@@ -8,34 +8,59 @@ class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
 
 
-        
-        
-        prev = None
-
         start = head
+        end = head
 
-        while head != None:
+        #need to account for when end fewer than k nodes left
 
-            tail = head
-            curr = tail         
+        #global head
+        global_head = start
 
-            index = k
+        previous_tail = None
+     
+        while start:
 
-            while curr and index > 0:
+            end = start
 
-                next_node = curr.next
-                curr.next = prev
-                prev = curr
-                curr = next_node
-                index -= 1
+            few_nodes = False
 
-            prev = tail
-            head = tail.next
+            for i in range(k):
+                if not end:
+                    few_nodes = True
+                    break           
+                end = end.next
+            
+            if few_nodes:
+                break
+            
+            initial = start
 
-        return head
-                
+            
+            prev = end
 
+            while start != end:
+                temp = start.next
+                start.next = prev
+                prev = start
+                start = temp
 
+            # prev is now the head of the reversed group
+
+            # First reversed group
+            if previous_tail is None:
+                global_head = prev
+            else:
+                previous_tail.next = prev
+
+            # initial used to be the first node,
+            # now it is the tail
+            previous_tail = initial
+
+            # start is already end,
+            # which is the beginning of next group
+            end = start
+
+        return global_head
 
 
         
